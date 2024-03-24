2b7b2baa8d69dae8ac8d1c232401856d42543e4f
anuvindajayan <anuvindajayan@gmail.com>
2024-03-24 22:43:58 +0530

    Initial commit

---
+++ android/app/src/main/java/com/newsapplication/newsapplication/MainActivity.java
@@ -0,0 +1,6 @@
+package com.newsapplication.newsapplication;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:newsapplication/controller/bottom_nav_control.dart';
+import 'package:newsapplication/controller/category_controller.dart';
+import 'package:newsapplication/controller/home_screen_controller.dart';
+import 'package:newsapplication/controller/search_screen_controller.dart';
+import 'package:newsapplication/view/bottom_nav/mainScreen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyNewsapp());
+}
+
+class MyNewsapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(providers: [
+      ChangeNotifierProvider(create: (context) => HomeScreenController()),
+      ChangeNotifierProvider(create: (context) => SearchScreenController()),
+      ChangeNotifierProvider(create: (context) => BottomNavController()),
+      ChangeNotifierProvider(create: (context) => CategoryController())
+    ],child: MaterialApp(debugShowCheckedModeBanner: false,home: Main_Screen(),),);
+  }
+}
